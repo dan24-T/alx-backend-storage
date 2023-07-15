19b425ecbb72cc82498e1afb45ea69236a68ac41
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-07-16 00:42:42 +0300

    Update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,14 @@
 -- Creates a function SafeDiv that divides
--- (and returns) the first number by the
--- second number or returns 0 if the second
--- number is 0
-DELIMITER $
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (
+a INT,
+b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    DECLARE res FLOAT DEFAULT 0;
-    IF b != 0 THEN
-        SET res = a / b;
-    END IF;
-    RETURN res;
-END$
-DELIMITER ;
+    RETURN (IF (b = 0, 0, a / b));
+END //
+
+DELIMITER;

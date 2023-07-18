e181cb6fc845de043ab74cc8c8b49e9c5d779bf6
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-07-18 23:01:48 +0300

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """
-PyMongo operations: updating documents
+updating documents
 """
 
 
 def update_topics(mongo_collection, name, topics):
     """
-    Updates topics field with given topics for
-    documents whose name field matches given name
+    Updates topics
     """
     mongo_collection.update_many({'name': name},
                                  {'$set': {'topics': topics}})

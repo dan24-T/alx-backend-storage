1239f2807c2ab2a4e24b95b8d93632b5fe5b17ca
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-07-18 22:48:50 +0300

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 """
-PyMongo operations: inserting new documents
+ inserting new documents
 """
 
-
 def insert_school(mongo_collection, **kwargs):
-    """ Inserts a new document in a mongodb collection
-        based on on kwargs
-        Return: id generated for inserted document
+    """ Inserts a new document
     """
     return mongo_collection.insert_one(kwargs).inserted_id

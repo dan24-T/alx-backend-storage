50697e0bdca6d710be3c2d6500b577bdd49f4ed6
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-07-18 23:02:31 +0300

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """
-PyMongo operations: matching data in list
+matching data in list
 """
-
 
 def schools_by_topic(mongo_collection, topic):
     """
-    Finds a list of school having a specific topic
+    Find specific topic
     """
     return mongo_collection.find({'topics': topic})
